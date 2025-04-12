fba9168976b5487d35b9d0614589ecf3dac9a03a
Mike <mxbalogun@gmail.com>
2025-04-12 00:28:04 -0400

    Added components and imported json

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import data from "./WebsiteData/data.json";
+import Navbar from './components/NavbarComponent/navbar';
+import About from './components/AboutComponent/about';
+import Experience from './components/ExperienceComponent/experience';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Work in progres....</h1>
+      <Navbar />
+      <About data={data.about}/>
     </>
   )
 }
 
-export default App
+export default App;
