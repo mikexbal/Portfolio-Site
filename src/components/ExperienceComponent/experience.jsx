bcd9ef091c55c45fc12944c6f3007a533537d57c
Mike <mxbalogun@gmail.com>
2025-04-12 13:27:21 -0400

    added section and header

--- src/components/ExperienceComponent/experience.jsx
+++ src/components/ExperienceComponent/experience.jsx
@@ -4,23 +4,29 @@ import styles from "./experience.module.css";
 function Experience({ data }) {
     return (
         <>
-            {data.map((item, index) => (
-                <div key={index} id={styles.experienceCard}>
-                    <div id={styles.experienceHeader}>
-                        <h1>{item.company}</h1>
-                        <p id={styles.experienceDate}>{item.date}</p>
+        <section id={styles.experienceSection}>
+            <h1 className="sectionHeader">Experience.</h1>
+                
+                {data.map((item, index) => (
+                    <div key={index} id={styles.experienceCard}>
+                        <div id={styles.experienceHeader}>
+                            <h1>{item.company}</h1>
+                            <p id={styles.experienceDate}>{item.date}</p>
+                        </div>
+
+                        <h3 id={styles.experienceRole}>{item.role}</h3>
+                        <h5 id={styles.experienceResponsibilities}>Responsibilities: </h5>
+
+                        <ul>
+                            {item.responsibilities.map((res, i) => (
+                                <li id={styles.listItem} key={i}>{res}</li>
+                            ))}
+                        </ul>
                     </div>
 
-                    <h3 id={styles.experienceRole}>{item.role}</h3>
-                    <h5 id={styles.experienceResponsibilities}>Responsibilities: </h5>
 
-                    <ul>
-                        {item.responsibilities.map((res, i) => (
-                            <li key={i}>{res}</li>
-                        ))}
-                    </ul>
-                </div>
-            ))}
+                ))}
+            </section>
         </>
     );
 }
