92db65796b053b8a0321fdfe2ba40da712438812
Mike <mxbalogun@gmail.com>
2025-04-12 13:26:55 -0400

    Added section to component

--- src/components/AboutComponent/about.jsx
+++ src/components/AboutComponent/about.jsx
@@ -6,16 +6,18 @@ function About({ data }) {
     return (
 
         <>
-            <div id={styles.introduction}>
-                <h1>{data.name}</h1>
-                <h3>{data.jobTitle}</h3>
-                <p>{data.bio}</p>
+            <section id={styles.aboutSection}>
+                <div id={styles.introduction}>
+                    <h1>{data.name}</h1>
+                    <h3>{data.jobTitle}</h3>
+                    <p>{data.bio}</p>
 
-                <div className={styles.buttonGroup}>
-                    <button id={styles.aboutButton}>Contact Me</button>
-                    <button id={styles.aboutButton}>My Work &#8594;</button>
+                    <div className={styles.buttonGroup}>
+                        <button id={styles.aboutButton}>Contact Me</button>
+                        <button id={styles.aboutButton}>My Work &#8594;</button>
+                    </div>
                 </div>
-            </div>
+            </section>
 
         </>
     )
